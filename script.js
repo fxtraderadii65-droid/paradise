b848cbb558c7cd4785bfcfa7ba3665b9283a7dee
fxtraderadii65-droid <fxtraderadii65@gmail.com>
2026-05-11 01:40:45 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+/* SCROLL ANIMATION */
+
+const cards = document.querySelectorAll('.course-card');
+
+window.addEventListener('scroll',()=>{
+
+cards.forEach(card=>{
+
+const top = card.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 100){
+
+card.style.opacity = "1";
+card.style.transform = "translateY(0px)";
+
+}
+
+});
+
+});
+
+/* INITIAL */
+
+cards.forEach(card=>{
+
+card.style.opacity = "0";
+card.style.transform = "translateY(50px)";
+card.style.transition = "1s";
+
+});
